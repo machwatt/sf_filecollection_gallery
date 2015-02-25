ed8777cbf67a52c66d066ad3c18d20cea0a15396
Jöran Kurschatke <j.kurschatke@skyfillers.com>
2015-02-25 17:33:26 +0100

    closes #16 - [TASK] make extension compatible with CMS 7.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.1.1',
+	'version' => '1.1.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.0.99',
+			'typo3' => '6.2.0-7.1.99',
 		),
 		'conflicts' => array(
 		),
