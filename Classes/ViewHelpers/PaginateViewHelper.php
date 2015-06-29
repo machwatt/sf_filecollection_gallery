dba9288099f8c4948d5596c20ad5e8e55280fb94
--global <j.kurschatke@skyfillers.com>
2015-06-29 08:55:38 +0200

    [TASK] Adjust copyrights to fit TYPO3CMS CGL

--- Classes/ViewHelpers/PaginateViewHelper.php
+++ Classes/ViewHelpers/PaginateViewHelper.php
@@ -1,6 +1,6 @@
 <?php
 namespace SKYFILLERS\SfFilecollectionGallery\ViewHelpers;
-/**
+/*
  * This file is part of the TYPO3 CMS project.
  *
  * It is free software; you can redistribute it and/or modify it under
