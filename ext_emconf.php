600510c133ba28e3b0029a5fc21acc9c17e97df0
--global <j.kurschatke@skyfillers.com>
2015-09-03 11:39:35 +0200

    [TASK] Make extension compatible for 7.4 closes #28

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.1.4',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.1.99',
+			'typo3' => '6.2.0-7.4.99',
 		),
 		'conflicts' => array(
 		),
