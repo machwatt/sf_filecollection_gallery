2bec82dc1481f7a96a5498a3c9e660a30d9c205f
Torben Hansen <derhansen@gmail.com>
2017-09-15 19:18:06 +0200

    Updated version dependencies in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,22 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'FileCollection Gallery',
-	'description' => 'Simple FileCollection Gallery',
-	'category' => 'plugin',
-	'author' => 'Jöran Kurschatke',
-	'author_email' => 'j.kurschatke@skyfillers.com',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '1.2.3',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0 - 8.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'FileCollection Gallery',
+    'description' => 'Simple FileCollection Gallery',
+    'category' => 'plugin',
+    'author' => 'Jöran Kurschatke',
+    'author_email' => 'joeran.kurschatke@gmail.com',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.2.3',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0 - 8.7.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
 );
