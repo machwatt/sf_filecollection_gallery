ee053fe450200df498be81c3d848f95101c3c278
Jöran Kurschatke <info@joerankurschatke.de>
2020-12-02 22:25:59 +0100

    [!!!][WIP][TASK] Split switchable controller actions

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,16 +7,45 @@ defined('TYPO3_MODE') or die();
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'sf_filecollection_gallery',
     'Pifilecollectiongallery',
-    'FileCollection Gallery'
+    'FileCollection Gallery (List View)'
+);
+
+/**
+ * Register the plugin
+ */
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'sf_filecollection_gallery',
+    'Pifilecollectiongallerynested',
+    'FileCollection Gallery (Nested View)'
+);
+
+/**
+ * Register the plugin
+ */
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'sf_filecollection_gallery',
+    'Pifilecollectiongalleryfolder',
+    'FileCollection Gallery (Nested Folders)'
 );
 
 /**
  * Add flexform
  */
-$pluginSignature = 'sffilecollectiongallery_pifilecollectiongallery';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['sffilecollectiongallery_pifilecollectiongallery'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $pluginSignature,
+    'sffilecollectiongallery_pifilecollectiongallery',
+    'FILE:EXT:sf_filecollection_gallery/Configuration/FlexForms/Flexform_plugin.xml'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['sffilecollectiongallery_pifilecollectiongallerynested'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'sffilecollectiongallery_pifilecollectiongallerynested',
+    'FILE:EXT:sf_filecollection_gallery/Configuration/FlexForms/Flexform_plugin.xml'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['sffilecollectiongallery_pifilecollectiongalleryfolder'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'sffilecollectiongallery_pifilecollectiongalleryfolder',
     'FILE:EXT:sf_filecollection_gallery/Configuration/FlexForms/Flexform_plugin.xml'
 );
 
@@ -32,4 +61,6 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
 /**
  * Remove unused fields
  */
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,recursive,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['sffilecollectiongallery_pifilecollectiongallery'] = 'layout,select_key,recursive,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['sffilecollectiongallery_pifilecollectiongallerynested'] = 'layout,select_key,recursive,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['sffilecollectiongallery_pifilecollectiongalleryfolder'] = 'layout,select_key,recursive,pages';
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,46 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'SKYFILLERS.' . $_EXTKEY,
-    'Pifilecollectiongallery',
-    [
-        'Gallery' => 'list, nested, nestedFromFolder, listFromFolder',
-    ],
-    // non-cacheable actions
-    [
-        'Gallery' => '',
+call_user_func(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'sf_filecollection_gallery',
+        'Pifilecollectiongallery',
+        [
+            'Gallery' => 'list',
+        ],
+        // non-cacheable actions
+        [
+            'Gallery' => '',
 
-    ]
-);
+        ]
+    );
 
-// Use hook from http://www.derhansen.de/2014/06/typo3-how-to-prevent-empty-flexform.html
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['SKYFILLERS.' . $_EXTKEY] = 'SKYFILLERS\SfFilecollectionGallery\Hooks\DataHandlerHooks';
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'sf_filecollection_gallery',
+        'Pifilecollectiongallerynested',
+        [
+            'Gallery' => 'list, nested',
+        ],
+        // non-cacheable actions
+        [
+            'Gallery' => '',
+
+        ]
+    );
+
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'sf_filecollection_gallery',
+        'Pifilecollectiongalleryfolder',
+        [
+            'Gallery' => 'nestedFromFolder, listFromFolder',
+        ],
+        // non-cacheable actions
+        [
+            'Gallery' => '',
+
+        ]
+    );
+
+    // Use hook from http://www.derhansen.de/2014/06/typo3-how-to-prevent-empty-flexform.html
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['SKYFILLERS.sf_filecollection_gallery'] = 'SKYFILLERS\SfFilecollectionGallery\Hooks\DataHandlerHooks';
+});
